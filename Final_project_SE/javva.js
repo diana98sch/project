030dfa34e184f3be792e83bd6c25ed1ed28b228f
Diana Schmidt <sch.diana98@gmail.com>
2018-11-30 15:28:34 +0200

    ProjektSE

---
+++ Final_project_SE/javva.js
@@ -0,0 +1,12 @@
+function opn(){
+	document.getElementById("p1").style.display='';
+	};
+	
+function opn2(){
+	document.getElementById("ol1").style.display='';
+    };
+	
+function opn3(){
+	document.getElementById("p2").style.display='';
+    };
+	
